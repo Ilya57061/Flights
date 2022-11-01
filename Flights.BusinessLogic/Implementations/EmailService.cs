de501a407cf48fcf498e2f196161538015dadd68
Ilya <81263807+Ilya57061@users.noreply.github.com>
2022-11-01 10:18:21 +0300

    Update EmailService.cs

--- Flights.BusinessLogic/Implementations/EmailService.cs
+++ Flights.BusinessLogic/Implementations/EmailService.cs
@@ -10,7 +10,7 @@ namespace Flights.BusinessLogic.Implementations
         {
             var emailMessage = new MimeMessage();
 
-            emailMessage.From.Add(new MailboxAddress("Travello", "johndavid57061@gmail.com"));
+            emailMessage.From.Add(new MailboxAddress("Travello", "xxxxxx"));
             emailMessage.To.Add(new MailboxAddress("", email));
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
@@ -21,7 +21,7 @@ namespace Flights.BusinessLogic.Implementations
             using (SmtpClient client = new SmtpClient())
             {
                 client.Connect("smtp.gmail.com", 465, true);
-                 client.Authenticate("johndavid57061@gmail.com", "cxhtzptdqmjeigeb");
+                 client.Authenticate("xxxxxx", "xxxxxxxxxx");
                 client.Send(emailMessage);
                  client.Disconnect(true);
             }
